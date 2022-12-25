82c2a31acc713b0e80c9fd5c449a8c36b8b566b9
AlinCodrut <69147352+AlinCodrut@users.noreply.github.com>
2022-12-25 15:36:05 +0100

    Update userRoutes.js

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -7,7 +7,7 @@ const { authenticateUser, authorizePermissions } = require("../middleware/authen
 // 1. Primul middleware (authenticateUser) verifica tokenul atunci cand userul da login
 // 2. al 2-lea middleware (authorizePermissions) verifica daca userul are acces de admin
 // 3. Controlerul ce face requeste (getAllUsers)
-route.get("/", getAllUsers)
+route.get("/",authenticateUser, authorizePermissions, getAllUsers)
 
 route.get("/showMe", showCurrentUsers) // conteaza ordinea ca cu asta generam id pentru urmatoarea ruta
 route.patch("/updateUser", updateUser)
